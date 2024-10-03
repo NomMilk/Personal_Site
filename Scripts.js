4af49cec74859f73b76bdd90ec435d872a20fa04
Homan Qiu <thehuiq2006@gmail.com>
2024-10-03 09:11:41 -0700

    background added

--- Scripts.js
+++ Scripts.js
@@ -21,22 +21,19 @@ function switchPage(title, content)
 function createWindow(title, content, x, y, width, height)
 {
     const template = document.getElementById('Windows');
-    const clone = template.content.cloneNode(true); // Clone the template content
+    const clone = template.content.cloneNode(true);
 
-    // Set the title and content
     const windowsTitle = clone.querySelector('.WindowsTitle');
     const windowsContent = clone.querySelector('.WindowsContent');
 
     windowsTitle.textContent = title;
     windowsContent.innerHTML = content;
 
-    // Get the window box (outer div) and set its style for dynamic positioning and size
     const windowsBox = clone.querySelector('.WindowsBox');
     windowsBox.style.marginLeft = `${x}px`;
     windowsBox.style.marginTop = `${y}px`;
     windowsBox.style.width = `${width}px`;
     windowsBox.style.height = `${height}px`;
 
-    // Append the clone to the main container
     document.getElementById('windowsContainer').appendChild(clone);
 }
