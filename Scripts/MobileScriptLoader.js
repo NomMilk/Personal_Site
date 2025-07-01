b44ca6671a562f9a724c7ecbfb898b2814eef38c
Fish <thehuiq2006@gmail.com>
2025-06-30 18:54:04 -0700

    mMBOIELBE

--- Scripts/MobileScriptLoader.js
+++ Scripts/MobileScriptLoader.js
@@ -2,11 +2,15 @@ function isMobileDevice() {
     return /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 }
 
-if (isMobileDevice())
-{
-    console.log("This is a mobile device.");
-}
-else
-{
-    console.log("This is not a mobile device.");
+if (isMobileDevice()) {
+    console.log("test");
+    const linkTag = document.getElementById("global-style");
+    if (linkTag) {
+        linkTag.href = "/Css/mobile/style.css";
+    }
+
+   const mobilelinkTag = document.getElementById("cursor-style");
+    if (mobilelinkTag) {
+        mobilelinkTag.href = "/Css/mobile/cursor.css";
+    }
 }
