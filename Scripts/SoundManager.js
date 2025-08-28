022920be646155a5569bc113660c5a9d5e733a28
Fish <thehuiq2006@gmail.com>
2025-08-27 18:35:52 -0700

    Music workin

--- Scripts/SoundManager.js
+++ Scripts/SoundManager.js
@@ -2,3 +2,17 @@ function PlaySound(_sound) {
     const audio = new Audio(_sound);
     audio.play();
 }
+
+let currentMusic = null;
+
+function PlayMusic(_music){
+    if (currentMusic == null) {
+        currentMusic = new Audio(_music);
+        currentMusic.loop = true;
+        currentMusic.play();
+    }
+    else {
+        currentMusic.pause();
+        currentMusic = null; 
+    }
+}
